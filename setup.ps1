adbacfbc79371c30b157d4981cad2ef956f554d1
Drew Burlingame <drewburlingame@gmail.com>
2012-12-22 14:58:56 -0800

    add setup.ps1 and install chocolatey

---
+++ setup.ps1
@@ -0,0 +1,18 @@
+iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
+
+$env:Path = $env:Path + ";c:\chocolatey\bin"
+
+cinst ruby
+cinst python
+cinst git-credential-winstore
+cinst gitextensions
+cinst curl
+cinst cmdaliases
+cinst notepadplusplus.install
+cinst virtualclonedrive
+cinst 7zip.install
+cinst webpicommandline
+cinst nuget.commandline
+
+chocolatey sources add -name drew-nuget -source "https://github.com/drewburlingame/setup-windows/nuget"
+chocolatey sources add -name daptiv-nuget -source "https://github.com/daptiv/setup-windows"
