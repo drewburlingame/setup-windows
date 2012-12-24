eb2e192919db4257f251fc0a85bd610829353036
Drew Burlingame <drewburlingame@gmail.com>
2012-12-23 22:51:54 -0800

    adding a lot of tools

--- setup.ps1
+++ setup.ps1
@@ -6,17 +6,27 @@ iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
 
 $env:Path = $env:Path + ";c:\chocolatey\bin"
 
+#boxstarter = 
+#  install windows updates
+#  disable: shutdown tracker, IE enhanced security config
+#  enable: remote desktop, 
+#  set explorer options, small taskbar
+cinst jivkok.boxstarter1
 cinst ruby
 cinst python
 cinst git-credential-winstore
-cinst gitextensions
 cinst curl
 cinst cmdaliases
 cinst 7zip.install
+cinst 7zip.commandline
 cinst notepadplusplus.install
 cinst virtualclonedrive
 cinst webpicommandline
 cinst nuget.commandline
+cinst linkshellextension
+cinst greenshot
+cinst teamviewer
+cinst swissfileknife
 
 #The OS gem allows for some easy telling if you’re on windows or not.
 cinst os -source ruby
